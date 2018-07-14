712fae5c26d4c3182d281c01a972bb6cce3a9a02
Kenneth Lee <liguozhu@hisilicon.com>
2018-07-14 21:29:33 +0800

    crypto: add hisilicon spimdev dummy driver
    The dummy driver is used to demostrate how spimdev works. It is simply a
    software module without IOMMU. So it works in NOIOMMU mode.
    
    This driver is used only on RFC patchset. It is not intended to be used
    for formal upstreaming.
    
    Signed-off-by: Kenneth Lee <liguozhu@hisilicon.com>

--- include/uapi/linux/vfio_spimdev.h
+++ include/uapi/linux/vfio_spimdev.h
@@ -13,6 +13,28 @@
 /* Device ATTRs in parent dev SYSFS DIR */
 #define VFIO_SPIMDEV_PDEV_ATTRS_GRP_NAME	"params"
 
+/* Parent device attributes */
+#define SPIMDEV_IOMMU_TYPE	"iommu_type"
+#define SPIMDEV_DMA_FLAG	"dma_flag"
+
+/* Maximum length of algorithm name string */
+#define VFIO_SPIMDEV_ALG_NAME_SIZE		64
+
+/* the bits used in SPIMDEV_DMA_FLAG attributes */
+#define VFIO_SPIMDEV_DMA_INVALID		0
+#define	VFIO_SPIMDEV_DMA_SINGLE_PROC_MAP	1 
+#define	VFIO_SPIMDEV_DMA_MULTI_PROC_MAP		2
+#define	VFIO_SPIMDEV_DMA_SVM			4
+#define	VFIO_SPIMDEV_DMA_SVM_NO_FAULT		8
+#define	VFIO_SPIMDEV_DMA_PHY			16
+
+/* Input data buffer can be a scatter-gather list */
+#define VFIO_SPIMDEV_CAPA_SGL		1
+
+/* Share whole process space with WD device */
+#define VFIO_SPIMDEV_CAPA_SHARE_ALL		2
+
+
 #define VFIO_SPIMDEV_CMD_WAIT	_IOW('W', 1, unsigned long)
 #define VFIO_SPIMDEV_CMD_GET_Q	_IOW('W', 2, unsigned long)
 #endif
