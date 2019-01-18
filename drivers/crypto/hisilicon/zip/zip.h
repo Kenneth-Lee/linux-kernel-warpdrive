5e5908dfa0479f4a4d0b29274f3f1711a0fe66d2
Kenneth Lee <liguozhu@hisilicon.com>
2019-01-18 23:12:42 +0800

    crypto/hisilicon/zip: add Hi1620CS support to hisilicon zip
    Signed-off-by: Kenneth Lee <liguozhu@hisilicon.com>
    Signed-off-by: Zhou Wang <wangzhou1@hisilicon.com>
    Signed-off-by: Hao Fang <fanghao11@huawei.com>

--- drivers/crypto/hisilicon/zip/zip.h
+++ drivers/crypto/hisilicon/zip/zip.h
@@ -1,57 +1,29 @@
-/* SPDX-License-Identifier: GPL-2.0+ */
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (c) 2018 Hisilicon Limited. */
 #ifndef HISI_ZIP_H
 #define HISI_ZIP_H
 
 #include <linux/list.h>
 #include "../qm.h"
+#include "zip_usr_if.h"
 
-#define HZIP_SQE_SIZE			128
-#define HZIP_SQ_SIZE			(HZIP_SQE_SIZE * QM_Q_DEPTH)
-#define QM_CQ_SIZE			(QM_CQE_SIZE * QM_Q_DEPTH)
-#define HZIP_PF_DEF_Q_NUM		64
-#define HZIP_PF_DEF_Q_BASE		0
+#undef pr_fmt
+#define pr_fmt(fmt)	"hisi_zip: " fmt
+
+enum hisi_zip_error_type {
+	/* negative compression */
+	HZIP_NC_ERR = 0x0d,
+};
+
+struct hisi_zip_ctrl;
 
 struct hisi_zip {
-	struct qm_info qm;
+	struct hisi_qm qm;
 	struct list_head list;
-
-#ifdef CONFIG_UACCE
-	struct uacce *uacce;
-#endif
+	struct hisi_zip_ctrl *ctrl;
 };
 
-struct hisi_zip_sqe {
-	__u32 consumed;
-	__u32 produced;
-	__u32 comp_data_length;
-	__u32 dw3;
-	__u32 input_data_length;
-	__u32 lba_l;
-	__u32 lba_h;
-	__u32 dw7;
-	__u32 dw8;
-	__u32 dw9;
-	__u32 dw10;
-	__u32 priv_info;
-	__u32 dw12;
-	__u32 tag;
-	__u32 dest_avail_out;
-	__u32 rsvd0;
-	__u32 comp_head_addr_l;
-	__u32 comp_head_addr_h;
-	__u32 source_addr_l;
-	__u32 source_addr_h;
-	__u32 dest_addr_l;
-	__u32 dest_addr_h;
-	__u32 stream_ctx_addr_l;
-	__u32 stream_ctx_addr_h;
-	__u32 cipher_key1_addr_l;
-	__u32 cipher_key1_addr_h;
-	__u32 cipher_key2_addr_l;
-	__u32 cipher_key2_addr_h;
-	__u32 rsvd1[4];
-};
-
-extern struct list_head hisi_zip_list;
-
+struct hisi_zip *find_zip_device(int node);
+int hisi_zip_register_to_crypto(void);
+void hisi_zip_unregister_from_crypto(void);
 #endif
